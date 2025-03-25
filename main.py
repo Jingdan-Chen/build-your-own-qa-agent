de9897de607affb7d3398d2e7cc9b9e0832c70a5
Haoyuan Zhang <32375424+haoyuanzhang123@users.noreply.github.com>
2025-03-25 09:53:27 +0000

    setup

---
+++ main.py
@@ -0,0 +1,10 @@
+"""React app."""
+
+from dataclasses import fields
+import types
+from typing import Callable, cast, Generator, Literal
+
+import mesop as me
+import react
+from datetime import datetime
+
